be18eb2a83ff8b375f02b5ac0caae0c82aafba3c
Unknown <Luv.Seewoosurran@city.ac.uk>
2018-04-05 02:14:50 +0100

    chat system

---
+++ src/chatsystem/chatsystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatsystem;
+
+import chatsystem.guifinal.receptiongui;
+
+/**
+ *
+ * @author Akash
+ */
+public class chatsystem {
+    
+    public static void main(String[] args){
+        
+        receptiongui reception = new receptiongui();
+        reception.show();
+    }
+    
+}
